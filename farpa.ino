c5b0985c17c10fb8ecedfed00244c6278c7cfd88
Tusty08 <98220751+Tusty08@users.noreply.github.com>
2025-11-19 15:29:10 -0300

    Create farpa.ino

---
+++ farpa.ino
@@ -0,0 +1,10 @@
+void Farpa(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+
+  delay(1500);

7964669f65f41fa6a4dae3e06e7bdd8e6c9adcff
Tusty08 <98220751+Tusty08@users.noreply.github.com>
2025-11-19 15:29:41 -0300

    Create regular.ino

---
+++ regular.ino
@@ -0,0 +1,12 @@
+void girarRegular(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+
+  delay(1500);
+
+}

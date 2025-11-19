e4a89af5223766f96f76659e122a45a5542dd392
Tusty08 <98220751+Tusty08@users.noreply.github.com>
2025-11-19 15:28:51 -0300

    Create Frente3.ino

---
+++ Frente3.ino
@@ -0,0 +1,12 @@
+void andarFrente3(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+
+  delay(1500);
+
+}

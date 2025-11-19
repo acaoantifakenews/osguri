3839cd0d1a816578b294a8296461ba1dba5a7a2c
Tusty08 <98220751+Tusty08@users.noreply.github.com>
2025-11-19 15:29:27 -0300

    Create angulo.ino

---
+++ angulo.ino
@@ -0,0 +1,10 @@
+void girarAngulo(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 150);
+
+  delay(1500);

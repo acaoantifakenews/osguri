bae7a8a3782a83997220a98c6e2cbec0ed2cfd76
Tusty08 <98220751+Tusty08@users.noreply.github.com>
2025-11-19 15:28:03 -0300

    Update Frente_P.ino

--- Frente_P.ino
+++ Frente_P.ino
@@ -1,1 +1,12 @@
+void andarFrente2(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
 
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+
+  delay(2000);
+
+}
